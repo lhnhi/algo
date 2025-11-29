1a4dff2bfbd1cac4d001f4cb59fe6cf62a97930e
lykimq <lykimq@gmail.com>
2025-11-29 17:18:52 +0100

    add phan sans_cube thieu

--- src/equation.py
+++ src/equation.py
@@ -13,3 +13,20 @@ def complexe(a, b, c):
         return {-c / b}
 
     return set()
+
+
+
+def sans_cube(n):
+    """
+    Test if n is not divisible by any number of type p³ where p >= 2 is an integer.
+    """
+    p = 2
+    while n != 1:
+        if n % p == 0:
+            if n % (p**3) == 0:
+                return False
+            n = n // p
+            if n % p == 0:
+                n = n // p
+        p = p + 1
+    return True
